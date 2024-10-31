f25efc9808047301a028807c3c26acf4c9870b71
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-10-31 00:24:32 +0000

    Add xp_level to AppUser table

--- database/migrations/2024_10_30_225746_create_app_users_table.php
+++ database/migrations/2024_10_30_225746_create_app_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('user_id')->unsigned();
             $table->string('username')->unique();
+            $table->bigInteger('xp_level')->unsigned();
             $table->timestamps();
 
 
