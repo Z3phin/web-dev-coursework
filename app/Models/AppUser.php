6294398096abc2045bceefe82b40abc851d92691
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-10-31 00:01:39 +0000

    Update AppUser to not extend User

--- app/Models/AppUser.php
+++ app/Models/AppUser.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class AppUser extends User
+class AppUser extends Model
 {
     use HasFactory;
 
