4ec21b31a966b565239b8c1ba0c3942435b853d3
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-10-31 01:42:34 +0000

    Add pronouns attribute to AppUser

--- database/migrations/2024_10_30_225746_create_app_users_table.php
+++ database/migrations/2024_10_30_225746_create_app_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('user_id')->unsigned();
             $table->string('username')->unique();
+            $table->string('pronouns');
             $table->bigInteger('xp_count')->unsigned();
             $table->enum('level', [
+                'gamer',
                 'student',
                 'trainee',
                 'junior',
