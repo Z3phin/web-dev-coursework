0e2de74d0766d5230aef9eb7413540d201114a4b
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-10-31 14:34:18 +0000

    Revert to before commentables was made

--- app/Models/AppUser.php
+++ app/Models/AppUser.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AppUser extends Model
 {
@@ -13,5 +14,6 @@ class AppUser extends Model
     public function user() : BelongsTo {
         return $this->belongsTo(User::class);
     }
+
 }
 
