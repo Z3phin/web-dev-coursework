8443367ccfa569e9467da3ca75fe603a2851b147
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-10-31 00:09:01 +0000

    Update AppUser username attribute to be unique

--- database/migrations/2024_10_30_225746_create_app_users_table.php
+++ database/migrations/2024_10_30_225746_create_app_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('app_users', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id')->unsigned();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->timestamps();
 
 
