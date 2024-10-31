9409c72f1108d644231d32763e85104028f9cafc
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-10-31 02:26:24 +0000

    Create basic seeder for AppUser

---
+++ database/seeders/AppUsersSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\AppUser;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AppUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = "Matthew Prickett";
+        $user->email = "Matt@email.com";
+        $user->password = "password";
+        $user->save();
+
+        $appUser = new AppUser();
+        $appUser->user_id = $user->id;
+        $appUser->username = "Zephin"; 
+        $appUser->pronouns = "he\him";
+        $appUser->status = "Currenty seeding";
+        $appUser->about = "I am the person who made this :) I do stuff.";
+        $appUser->xp_count = 800;
+        $appUser->level = 'student';
+        $appUser->last_online = $user->created_at;
+        $appUser->save();
+
+    }
+}
