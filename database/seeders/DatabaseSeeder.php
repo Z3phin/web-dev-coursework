7defb5bb78506422543a7e6655643358d593eea8
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-01 03:16:54 +0000

    Create PostsTableSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         $this->call(AppUsersSeeder::class);
+        $this->call(PostsTableSeeder::class);
         
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\Activity;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $post = new Post;
+        $post->title = "Test post title";
+        $post->save();
+
+        $activity = new Activity;
+        $activity->app_user_id = 1;
+        $activity->commentable_type='App\\Models\\Post';
+        $activity->commentable_id=$post->id;
+        $activity->body="Test body for post";
+        $activity->like_count=10;
+        $activity->dislike_count=100;
+        $activity->save();
+
+    }
+}
