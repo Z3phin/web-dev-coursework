3c5f4ed2d35dec8beaccf9a554b9df084fcf5a3e
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-01 22:50:43 +0000

    Add Forums table columns

--- database/migrations/2024_11_01_224123_create_forums_table.php
+++ database/migrations/2024_11_01_224123_create_forums_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('forums', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('owner_id');
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
