7e96d13fee77629d5d9812a4329bb307171a5e37
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-01 15:50:32 +0000

    Add comment seeder

--- database/seeders/CommentsTableSeeder.php
+++ database/seeders/CommentsTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Activity;
+use App\Models\Comment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,31 @@ class CommentsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $comment = new Comment;
+        $comment->parent_activity_id=1; // test post with id of 1
+        $comment->save();
+
+        $activity = new Activity;
+        $activity->app_user_id=2;
+        $activity->commentable_type="App\Models\Comment";
+        $activity->commentable_id=$comment->id;
+        $activity->body="This is a comment\'s body";
+        $activity->like_count=100;
+        $activity->dislike_count=10;
+        $activity->save();
+
+
+        $commentOnComment = new Comment;
+        $commentOnComment->parent_activity_id=$comment->activity->id;
+        $commentOnComment->save();
+
+        $activity2 = new Activity;
+        $activity2->app_user_id=3;
+        $activity2->commentable_type="App\Models\Comment";
+        $activity2->commentable_id=$commentOnComment->id;
+        $activity2->body="This is a comment\'s body";
+        $activity2->like_count=100;
+        $activity2->dislike_count=10;        
+        $activity2->save();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(AppUsersSeeder::class);
         $this->call(PostsTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
         
     }
 }
