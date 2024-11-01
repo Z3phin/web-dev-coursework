41c30ea840099c36290e0bdd00c2e52eb39088f0
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-01 02:36:37 +0000

    Update comment and activity to reflect one-to-many relationship

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Activity extends Model
@@ -17,5 +18,9 @@ class Activity extends Model
 
     public function commentable() : MorphTo {
         return $this->morphTo('commentable');
+    }
+
+    public function comments() : HasMany {
+        return $this->hasMany(Comment::class, 'parent_activity_id', 'id');
     }
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Comment extends Activity 
@@ -11,5 +12,9 @@ class Comment extends Activity
 
     public function activity() : MorphOne {
         return $this->morphOne(Activity::class, 'commentable');
+    }
+
+    public function parent_activity() : BelongsTo {
+        return $this->belongsTo(Activity::class, 'id', 'parent_activity_id');
     }
 }
