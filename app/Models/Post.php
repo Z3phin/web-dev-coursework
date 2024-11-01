ac5a22b0fa5f3f41978671d532b83ff9ad87126c
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-01 16:47:16 +0000

    Update Post with comments() which has many through activity

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Post extends Model
@@ -12,5 +13,16 @@ class Post extends Model
 
     public function activity() : MorphOne {
         return $this->morphOne(Activity::class, 'commentable');
+    }
+
+    public function comments() : HasManyThrough {
+        return $this->hasManyThrough(
+            Comment::class, 
+            Activity::class,
+            'commentable_id',
+            'parent_activity_id',
+            'id',
+            'id'
+        );
     }
 }
