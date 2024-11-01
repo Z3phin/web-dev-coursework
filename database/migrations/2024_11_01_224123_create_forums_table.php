2a79b3aeb38bd09c50cded3e740a6067b5509e35
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-01 23:02:54 +0000

    Add foreign key relationship with AppUser to Forum table

--- database/migrations/2024_11_01_224123_create_forums_table.php
+++ database/migrations/2024_11_01_224123_create_forums_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('forums', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('owner_id');
+            $table->foreignId('owner_id')->nullable();
             $table->string('name');
             $table->text('description');
             $table->timestamps();
+            
+            $table->foreign('owner_id')->references('id')->on('user_id')
+                ->onUpdate('cascade')->onDelete('set null');
         });
     }
 
