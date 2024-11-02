997f042c5b42b7c5e9dd4d228a00f0bf93364077
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-02 05:53:37 +0000

    Add comments to Comment ;)

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,18 +8,39 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
+
+/**
+ * Represents a comment on a commentable activity, including a post or another comment.
+ * 
+ */
 class Comment extends Model 
 {
     use HasFactory;
 
+    /**
+     * Returns the main activity this comment is related to.
+     * 
+     * @return MorphOne activity.
+     */
     public function activity() : MorphOne {
         return $this->morphOne(Activity::class, 'commentable');
     }
 
+    /**
+     * Returns the parent activity, which may be a post or a comment, that this 
+     * comment belongs to.
+     * 
+     * @return BelongsTo activity.
+     */
     public function parent_activity() : BelongsTo {
         return $this->belongsTo(Activity::class, 'parent_activity_id');
     }
 
+    /**
+     * Returns the comments that belong to this comment.
+     * 
+     * @return HasManyThrough comments.
+     */
     public function comments() : HasManyThrough {
         return $this->hasManyThrough(
             Comment::class, 
