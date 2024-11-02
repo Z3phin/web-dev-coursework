0a4cd9c2f106c3cd37ee6b9b4530916dd32dfdf6
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-02 06:02:26 +0000

    Add comments to Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,14 +8,28 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
+/**
+ * Represents a post, which has a title, that has been made by a user.
+ * 
+ */
 class Post extends Model
 {
     use HasFactory;
 
+    /**
+     * Returns the activity that this Post has. This contains the main content of the post.
+     * 
+     * @return MorphOne activity.
+     */
     public function activity() : MorphOne {
         return $this->morphOne(Activity::class, 'commentable');
     }
 
+    /**
+     * Returns the comments that have been made on this post.
+     * 
+     * @return HasManyThrough comments.
+     */
     public function comments() : HasManyThrough {
         return $this->hasManyThrough(
             Comment::class, 
@@ -27,6 +41,11 @@ class Post extends Model
         );
     }
 
+    /**
+     * Returns the forum that this post was made to.
+     * 
+     * @return BelongsTo forum. 
+     */
     public function forum() : BelongsTo {
         return $this->belongsTo(Forum::class);
     }
