cf7048b3deb95ffb2aeb5e6092c508be4d1a74aa
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-02 02:55:43 +0000

    Update ForumSeeder to add random members

--- database/seeders/ForumSeeder.php
+++ database/seeders/ForumSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\AppUser;
 use App\Models\Forum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +21,33 @@ class ForumSeeder extends Seeder
         $forum->save();
 
         Forum::factory()->count(20)->create();
+
+        foreach (Forum::all() as $forum) {
+            // Generate random users ids to add to the forum 
+            $ids = fake()->randomElements(
+                range(1, AppUser::count()), 
+                fake()->numberBetween(0, 10)
+            );
+
+            // Ensure that the owner is not added as a member
+            $ids = array_diff($ids, [$forum->owner_id]);
+
+            // Add each user to the forum with a random role and random join date
+            for ($id = 0; $id < count($ids); $id++) {
+                $forum->members()->attach(
+                    $ids[$id],
+                    [
+                        'role' => fake()->randomElement(
+                            ['member', 'moderator']
+                        ),
+                        'joined_at' => fake()->dateTimeBetween(
+                            $forum->created_at,
+                            '+1 week'
+                        )
+                    ]
+                    );
+            } 
+        }
         
     }
 }
