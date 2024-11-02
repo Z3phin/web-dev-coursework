0cff10bb5c0d6d6ec6c778c95f2c4b7edce68eb9
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-02 05:59:26 +0000

    Add comments to Forum

--- app/Models/Forum.php
+++ app/Models/Forum.php
@@ -7,16 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
+/**
+ * Represents a forum, where users can put thier posts.
+ * A user owns a forum, and a forum has members which are users. 
+ * Those members may be moderators.
+ * Some users may be banned from the forum. 
+ * 
+ */
 class Forum extends Model
 {
     use HasFactory;
 
+    /**
+     * Returns the owner of this forum.
+     * 
+     * @return BelongsTo AppUser
+     */
     public function owner() : BelongsTo {
         return $this->belongsTo(AppUser::class);
     }
 
+    /**
+     * Returns the members that belong to this forum.
+     * 
+     * @return BelongsToMany users.
+     */
     public function members() : BelongsToMany {
         return $this->belongsToMany(
             AppUser::class,
@@ -28,11 +44,22 @@ class Forum extends Model
         );
     }
 
+    /**
+     * Returns the moderators for this forum.
+     * 
+     * @return BelongsToMany moderators.
+     */
     public function moderators() : BelongsToMany {
         return $this->members()->wherePivot('role', '=', 'moderator');
     }
 
-    public function bannedMembers() : BelongsToMany {
+    /**
+     * Returns the users who are banned from this forum.
+     * 
+     * @return BelongsToMany banned users.
+     * 
+     */
+    public function bannedUsers() : BelongsToMany {
         return $this->belongsToMany(
             AppUser::class,
             'banned_forum_users',
@@ -43,6 +70,11 @@ class Forum extends Model
         );
     }
 
+    /**
+     * Returns the posts that have been made on this forum.
+     * 
+     * @return HasMany posts.
+     */
     public function posts() : HasMany {
         return $this->hasMany(Post::class);
     }
