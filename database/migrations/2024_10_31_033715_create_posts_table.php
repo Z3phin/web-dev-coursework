6a54488e3867ad168fe7234276dfef5700881f35
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-09 16:42:55 +0000

    Add migrations (changed the times so they ran correclty)

---
+++ database/migrations/2024_10_31_033715_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('commentable');
+            $table->string('title');
+            $table->bigInteger('view_count')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('commentable_id')->references('id')->on('commentables')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2024_11_01_235501_forum_moderators.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forum_moderators', function (Blueprint $table){
+            $table->primary(['forum_id', 'app_user_id']);
+            $table->foreignId('forum_id');
+            $table->foreignId('app_user_id');
+
+            $table->foreign('forum_id')->references('id')->on('forums')
+                ->onUpdate('cascade')->onDelete('cascade');
+                
+            $table->foreign('app_user_id')->references('id')->on('app_users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forum_moderators');
+    }
+};
