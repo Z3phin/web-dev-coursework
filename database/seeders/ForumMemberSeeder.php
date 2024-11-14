1f4b573b4d78850050b9d8198f040c2058cbe312
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-14 02:28:11 +0000

    Update ForumMemberSeeder

--- database/seeders/ForumMemberSeeder.php
+++ database/seeders/ForumMemberSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Forum;
+use App\Models\AppUser;
 use Illuminate\Database\Seeder;
 
 class ForumMemberSeeder extends Seeder
@@ -12,6 +13,30 @@ class ForumMemberSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Add members to all the forums
+        foreach (Forum::all() as $forum) {
+            // Generate up to 10 random users ids to add to the forum 
+            $ids = fake()->randomElements(
+                range(1, AppUser::count()), 
+                fake()->numberBetween(0, 10)
+            );
+
+            // Ensure owner is not included in member pool
+            $ids = array_diff($ids, [$forum->owner_id]);
+
+            // Add each user to the forum with a random role and random join date
+            foreach ($ids as $id) {
+                $forum->members()->attach(
+                    $id,
+                    [
+                        'role' => fake()->randomElement(['member', 'moderator']),
+                        'joined_at' => fake()->dateTimeBetween(
+                            $forum->created_at,
+                            '+1 week'
+                        )
+                    ]
+                    );
+            } 
+        }
     }
 }
