06a04415b7111365bf1cfa1361b26551344c9250
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-14 03:11:22 +0000

    Create basic main app view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        <header>
+            <div style="background-color: black; height=1rem">
+
+            </div>
+        </header>
+        <section>
+            @yield('content')
+        </section>
+    </body>
+</html>
