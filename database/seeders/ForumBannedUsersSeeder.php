131c1b49197ad770acb1bf6c743a241b71462e9b
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-14 02:26:03 +0000

    Update ForumBannedUsersSeeder

--- database/seeders/ForumBannedUsersSeeder.php
+++ database/seeders/ForumBannedUsersSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Forum;
+use App\Models\AppUser;
 use Illuminate\Database\Seeder;
 
 class ForumBannedUsersSeeder extends Seeder
@@ -12,6 +13,36 @@ class ForumBannedUsersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Add members to all the forums
+        foreach (Forum::all() as $forum) {
+
+
+            // Generate up to 3 random user ids to be banned 
+            $bannedIDs = fake()->randomElements(
+                range(1, AppUser::count()), 
+                fake()->numberBetween(0, 3)
+            );
+
+            // Ensure generated bannedIDs are not members or owner
+            $bannedIDs = array_diff($bannedIDs, $forum->members(), [$forum->owner_id]);
+
+
+            // Add banned users to forums
+            foreach ($bannedIDs as $id) {
+
+                $bannedAt = fake()->dateTimeBetween($forum->created_at, '+1 year');
+
+                $bannedUntil = fake()->dateTimeBetween($bannedAt, '+2 years');
+
+                $forum->bannedUsers()->attach(
+                    $id,
+                    [
+                        'reason' => fake()->sentence(),
+                        'banned_at' => $bannedAt,
+                        'banned_until' => $bannedUntil
+                    ]
+                );
+            } 
+        }
     }
 }
