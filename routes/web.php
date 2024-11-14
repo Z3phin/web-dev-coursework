794c56c78900b3b7327281b12d9c399f8fb6319e
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-14 03:21:16 +0000

    Add a route to the forums

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ForumController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/forums', [ForumController::class, 'index'])->name('forums.index');
