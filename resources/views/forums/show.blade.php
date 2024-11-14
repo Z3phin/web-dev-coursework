bc1f90494cadc4c093d6e4e95b9572921a9c03f1
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-14 04:09:28 +0000

    Update forums show to display a basic list of post titles

--- resources/views/forums/show.blade.php
+++ resources/views/forums/show.blade.php
@@ -12,12 +12,19 @@
         <li>
             {{$forum->description}}
         </li>
+        <a href="{{route('forums.index')}}">Back to all forums</a>
     </ul>
     <hr>
     <section>
         <ul>
         @foreach ($forum->posts as $post)
-            <li>{{$post->title}}</li>
+            <li>
+                {{$post->title}}
+                <ul>
+                    <li>{{$post->activity->appUser->username}}</li>
+                    <li>{{$post->created_at}}</li>
+                </ul>
+            </li>
         @endforeach
         </ul>
     </section>
