dd989a74c3e5aa3c587ae679adb9ac02aabfd321
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-27 22:12:27 +0000

    Remove broken hover styling

--- resources/views/forums/index.blade.php
+++ resources/views/forums/index.blade.php
@@ -22,7 +22,6 @@
     <div class="py-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 " >
             <div class="bg-white
-             {{-- hover:bg-slate-400  --}}
              dark:bg-gray-800    
                overflow-hidden 
                shadow-sm 
