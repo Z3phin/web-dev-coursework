b4dc2ff55c715b6ec5af0b133e3ca90b2772f6af
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-29 02:42:11 +0000

    Update creating a user to also make an AppUser

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\AppUser;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -33,6 +34,9 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'username' => ['required', 'string', 'max:255', 'unique:'.AppUser::class],
+            'pronouns' => 'string',
+            'level' => 'string'
         ]);
 
         $user = User::create([
@@ -40,6 +44,25 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $pronouns = ($request->pronouns == 'unselected' 
+                    || $request->pronouns == 'withheld'
+                    || $request->pronouns == null) 
+                    ? 'unselected' 
+                    : $request->pronouns;
+
+        $level = ($request->level == null) ? 'gamer' : $request->level;
+
+        $appUser = new AppUser();
+        $appUser->user_id = $user->id;
+        $appUser->username = $request->username;
+        $appUser->pronouns = $pronouns;
+        $appUser->status = 'Checking this app out';
+        $appUser->about = 'Hi! I am using this app!';
+        $appUser->xp_count = 0;
+        $appUser->last_online = $user->created_at;
+        $appUser->level = $level;
+        $appUser->save();
 
         event(new Registered($user));
 
