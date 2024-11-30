5334557bb9597431968f8fb791bffeed0af6b5a4
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-11-30 20:10:16 +0000

    Fix comment date wrongly displaying the parent post's date

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -74,7 +74,7 @@
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100 m-4">
                         <p class="py-2">
-                            {{'@' . $comment->activity->appUser->username . ' | ' . $post->created_at }}
+                            {{'@' . $comment->activity->appUser->username . ' | ' . $comment->created_at }}
                         </p>
                         <p>
                             {{$comment->activity->body}}
