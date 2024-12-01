a23b352608a8152e8a0fa740c074bd0a98141629
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-12-01 03:09:15 +0000

    Fix an issue with left over bad code on forums.index

--- resources/views/forums/index.blade.php
+++ resources/views/forums/index.blade.php
@@ -1,24 +1,9 @@
-{{-- @extends('layouts.app')
-
-@section('title', 'Forums')
-
-@section('content')
-    <ul>
-        @foreach ($forums as $forum)
-            <li><a href="{{route('forums.show', ['id' => $forum->id])}}">{{$forum->name}}</a></li>
-        @endforeach
-    </ul>
-@endsection --}}
-
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('All Forums') }}
         </h2>
     </x-slot>
-
-    @livewire('counter')
 
     @foreach ($forums as $forum)
     <div class="py-2">
