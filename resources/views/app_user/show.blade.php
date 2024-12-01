f2022fa83983f93335c1c1f4c2f7223fb23a943e
Matthew Prickett <matthew.prickett@hotmail.co.uk>
2024-12-01 03:28:57 +0000

    Add links to followers and following to a user's profile page

--- resources/views/app_user/show.blade.php
+++ resources/views/app_user/show.blade.php
@@ -5,13 +5,100 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Welcome to " . $appUser->username .  "'s page") }}
+    <div class="w-screen py-12 flex">
+        <div class="w-3/4 mx-6 flex-auto">
+            <div class="mx-6 px-6">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Welcome to " . $appUser->username .  "'s page") }}
+                    </div>
+                </div>
+    
+                <!-- Activity Feed --> 
+                
+                <div class="mt-4 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <section>
+                            <h1>Activity Feed</h1>
+                            <a href="{{route('appUser.show', ['appUser' => $appUser])}}">
+                                Overview
+                            </a>
+                            <a href="{{route('appUser.show', ['appUser' => $appUser])}}">
+                                Posts
+                            </a>
+                            <a href="{{route('appUser.show', ['appUser' => $appUser])}}">
+                                Comments
+                            </a>
+                        </section>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="w-1/4 flex-auto shrink-0 ">
+            <div class=" mx-6 pr-6">
+                <div class="bg-white dark:bg-gray-800 overflow-visible shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        
+                        <!-- Username -->
+                        <p class="text-center">{{$appUser->username}}</p>
+                        <!-- Follow/Followers -->
+                        <hr>
+                        <div class="flex">
+                            <span class="text-center flex-1">
+                                <x-dropdown>
+                                    <x-slot name="trigger">
+                                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                            <div>
+                                                <p>Following</p>
+                                                <p>{{$appUser->following->count()}}</p>
+                                            </div>
+                                        </button>
+                                    </x-slot>
+                                    <x-slot name="content">
+                                        @foreach ($appUser->following as $follow)
+                                            <x-dropdown-link :href="route('appUser.show', ['appUser' => $follow])">
+                                                {{$follow->username}}
+                                            </x-dropdown-link>
+                                        @endforeach
+                                    </x-slot>
+                                </x-dropdown>
+                            </span>
+                            <span class="text-center flex-1">
+                                <x-dropdown>
+                                    <x-slot name="trigger">
+                                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                            <div>
+                                                <p>Followers</p>
+                                                <p>{{$appUser->followers->count()}}</p>
+                                            </div>
+                                        </button>
+                                    </x-slot>
+                                    <x-slot name="content">
+                                        @foreach ($appUser->followers as $follower)
+                                            <x-dropdown-link :href="route('appUser.show', ['appUser' => $follow])">
+                                                {{$follower->username}}
+                                            </x-dropdown-link>
+                                        @endforeach
+                                    </x-slot>
+                                </x-dropdown>
+                            </span>
+                        </div>
+                        
+                        <!-- Status -->
+                        <!-- Description -->
+                        <!-- Stats and Facts -->
+                        <!-- Owns these Forums -->
+                        <!-- Moderator Of -->
+                        <!-- Member of -->
+                        
+
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
     </div>
+
+
 </x-app-layout>
